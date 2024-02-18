87189875e4f97a64c38b2d406e355c72d69e6de9
Ibrahima Ciss <bionik6@gmail.com>
2024-02-18 14:32:24 +0000

    Initial Commit

---
+++ MoviePlus/MoviePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviePlusApp.swift
+//  MoviePlus
+//
+//  Created by Ibrahima Ciss on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
