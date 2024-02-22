259b769014e456f1deb0ec0333092f430abf016b
Ibrahima Ciss <bionik6@gmail.com>
2024-02-22 17:01:37 +0000

    Add ColorToken

--- Packages/Sources/CoreUI/Theming/ColorToken.swift
+++ Packages/Sources/CoreUI/Theming/ColorToken.swift
@@ -1,2 +1,18 @@
 import SwiftUI
 
+enum ColorToken {
+  // Semantics
+  static let red100: Color = .hex("#F5853F")
+  static let blue100: Color = .hex("#3DA5D9")
+  static let green100: Color = .hex("#20BF55")
+  static let orange100: Color = .hex("#FE9920")
+
+  // Neutral
+  static let black100: Color = .hex("#020202")
+  static let black60: Color = .hex("#3F4045")
+  static let black20: Color = .hex("#9F9FA2")
+  static let white: Color = .hex("#FFFFFF")
+
+  // Background
+  static let blackBg100: Color = .hex("#0F1016")
+}
